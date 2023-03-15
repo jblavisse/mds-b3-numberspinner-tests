68e4df7f646e03cf563651deb336742b69d3add5
Jb <jb@levillageducode.fr>
2023-03-15 10:38:34 +0100

    Add increment and decrement methods in NumberSpinner

--- components/NumberSpinner.js
+++ components/NumberSpinner.js
@@ -3,6 +3,15 @@ class NumberSpinner {
     this.value = 0;
     this.step = 1;
   }
+
+  increment() {
+    this.value += this.step;
+  }
+
+  decrement() {
+    this.value = this.value - this.step;
+    // this.value -= this.step;
+  }
 }
 
 export default NumberSpinner;
--- components/NumberSpinner.test.js
+++ components/NumberSpinner.test.js
@@ -1,7 +1,7 @@
 import NumberSpinner from "./NumberSpinner";
 import { it, expect } from "vitest";
 
-it("1+1 doit faire 2", () => {
+it("Creating a spinner without attributes should be possible", () => {
   // 1. Déclarer le jeu de données
   // 2. Fais des trucs
   let numberSpinner = new NumberSpinner();
@@ -11,8 +11,24 @@ it("1+1 doit faire 2", () => {
   expect(numberSpinner.step).toBe(1);
 });
 
-it("blablabla", () => {
+it("Incrementing the spinner should add the step to the value", () => {
   // 1. Déclarer le jeu de données
+  // value = 0
+  let numberSpinner = new NumberSpinner();
+
   // 2. Fais des trucs
+  numberSpinner.increment();
+
   // 3. On vérifie si c'est conforme à ce qu'on veut
+  expect(numberSpinner.value).toBe(1);
+});
+
+it("Decrementing the spinner should substract the step to the value", () => {
+  let numberSpinner = new NumberSpinner();
+
+  numberSpinner.decrement();
+  expect(numberSpinner.value).toBe(-1);
+
+  numberSpinner.decrement();
+  expect(numberSpinner.value).toBe(-2);
 });
